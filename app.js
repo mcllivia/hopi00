7c0281e1d26c862a0a5e14ffae3ff64130bf3272
mari00ana <molygomes123@gmail.com>
2025-05-05 10:59:08 -0300

    comit 2

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
+// Rota para a página de dashboard
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
